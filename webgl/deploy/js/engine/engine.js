779baddb4fbd4034685b3719fa617afca683780c
Ziagl <office@ziegelwanger-edv.at>
2021-01-25 18:47:53 +0100

    draw triangle

--- webgl/deploy/js/engine/engine.js
+++ webgl/deploy/js/engine/engine.js
@@ -10,6 +10,7 @@ var webglEngine;
             this._canvas = webglEngine.GLUtilities.initialize();
             this._shader = this.loadShaders();
             this._shader.use();
+            this._buffer = this.createBuffer();
             webglEngine.gl.clearColor(0, 0, 0, 1);
         }
         /**
@@ -17,6 +18,7 @@ var webglEngine;
          */
         Engine.prototype.start = function () {
             var _this = this;
+            this.resize();
             // Create and start the game loop
             var gameloop = function () {
                 _this.update();
@@ -28,6 +30,7 @@ var webglEngine;
             if (this._canvas !== undefined) {
                 this._canvas.width = window.innerWidth;
                 this._canvas.height = window.innerHeight;
+                webglEngine.gl.viewport(0, 0, this._canvas.width, this._canvas.height);
             }
         };
         Engine.prototype.update = function () {
@@ -36,6 +39,27 @@ var webglEngine;
         };
         Engine.prototype.draw = function () {
             webglEngine.gl.clear(webglEngine.gl.COLOR_BUFFER_BIT);
+            // draw buffer
+            webglEngine.gl.bindBuffer(webglEngine.gl.ARRAY_BUFFER, this._buffer);
+            webglEngine.gl.vertexAttribPointer(0, 3, webglEngine.gl.FLOAT, false, 0, 0);
+            webglEngine.gl.enableVertexAttribArray(0);
+            webglEngine.gl.drawArrays(webglEngine.gl.TRIANGLES, 0, 3);
+        };
+        Engine.prototype.createBuffer = function () {
+            var buffer = webglEngine.gl.createBuffer();
+            var vertices = [
+                // x y z
+                -0.5, -0.5, 0,
+                0.5, -0.5, 0,
+                0, 0.5, 0
+            ];
+            webglEngine.gl.bindBuffer(webglEngine.gl.ARRAY_BUFFER, buffer);
+            webglEngine.gl.vertexAttribPointer(0, 3, webglEngine.gl.FLOAT, false, 0, 0);
+            webglEngine.gl.enableVertexAttribArray(0);
+            webglEngine.gl.bufferData(webglEngine.gl.ARRAY_BUFFER, new Float32Array(vertices), webglEngine.gl.STATIC_DRAW);
+            webglEngine.gl.bindBuffer(webglEngine.gl.ARRAY_BUFFER, null);
+            webglEngine.gl.disableVertexAttribArray(0);
+            return buffer;
         };
         Engine.prototype.loadShaders = function () {
             var vertexShaderSource = "\nattribute vec3 a_position;\n\nvoid main() \n{\n    gl_Position = vec4(a_position, 1.0);\n}";
--- webgl/src/engine/engine.ts
+++ webgl/src/engine/engine.ts
@@ -5,6 +5,8 @@ module webglEngine
         private _canvas:HTMLCanvasElement;
         private _shader:Shader;
 
+        private _buffer:WebGLBuffer;
+
         /**
          * initialize engine with canvas element
          * @param display 
@@ -14,6 +16,7 @@ module webglEngine
             this._canvas = GLUtilities.initialize();
             this._shader = this.loadShaders();
             this._shader.use();
+            this._buffer = this.createBuffer();
 
             gl.clearColor(0,0,0,1);
         }
@@ -23,6 +26,7 @@ module webglEngine
          */
         public start():void
         {
+            this.resize();
             // Create and start the game loop
             var gameloop = () => { 
                 this.update(); 
@@ -37,6 +41,8 @@ module webglEngine
             {
                 this._canvas.width = window.innerWidth;
                 this._canvas.height = window.innerHeight;
+
+                gl.viewport(0,0,this._canvas.width, this._canvas.height);
             }
         }
 
@@ -50,6 +56,32 @@ module webglEngine
         {
             gl.clear(gl.COLOR_BUFFER_BIT);
 
+            // draw buffer
+            gl.bindBuffer(gl.ARRAY_BUFFER, this._buffer);
+            gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 0, 0);
+            gl.enableVertexAttribArray(0);
+            gl.drawArrays(gl.TRIANGLES, 0, 3);
+            
+        }
+
+        private createBuffer():WebGLBuffer
+        {
+            let buffer:WebGLBuffer = <WebGLBuffer> gl.createBuffer();
+            let vertices = [
+                // x y z
+                -0.5, -0.5, 0,
+                0.5, -0.5, 0,
+                0, 0.5, 0
+            ];
+
+            gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+            gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 0, 0);
+            gl.enableVertexAttribArray(0);
+            gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+            gl.bindBuffer(gl.ARRAY_BUFFER, null);
+            gl.disableVertexAttribArray(0);
+
+            return buffer;
         }
 
         private loadShaders():Shader
